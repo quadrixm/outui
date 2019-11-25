f882c69415ce6ca05323d1d9d11df3d91a680178
Mohammad Quadri <quadrixm@gmail.com>
2019-11-25 12:59:22 +0530

    Adding basic ui blocks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <section className="section">
+        <div className="container">
+          <div className="columns">
+            <div className="column has-text-centered">
+              <h3>Select Employee</h3>
+              <div className="select">
+                <select>
+                  <option>Employee 1</option>
+                  <option>Employee 2</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <div className="columns">
+            <div className="column has-text-centered">
+              <h3>Survey List</h3>
+            </div>
+            <div className="column has-text-centered">
+              <h3>Assign Survey</h3>
+            </div>
+          </div>
+          <div className="columns">
+            <div className="column has-text-centered">
+              <button className="button is-primary">
+                Done
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
   );
 }
 
